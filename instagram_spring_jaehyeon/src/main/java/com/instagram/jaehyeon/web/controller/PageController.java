db5302c0538c35be7211de8278d32a1e3e7883d4
= <ITPS@gmail.com>
2022-05-06 12:57:02 +0900

    instagram get요청

--- instagram_spring_jaehyeon/src/main/java/com/instagram/jaehyeon/web/controller/PageController.java
+++ instagram_spring_jaehyeon/src/main/java/com/instagram/jaehyeon/web/controller/PageController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PageController {
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getIndex() {
+		return "index";
+	}
+	
 	@RequestMapping(value = "/auth/signup", method = RequestMethod.GET)
 	public String getSignup() {
 		return "auth/signup";
